141fef204aae0a8a1871bf2b88ec3f83ebfba5b2
Pedro Costa <40130589+pCosta99@users.noreply.github.com>
2022-06-14 18:41:45 +0100

    Cleaning of historic rates (#22)

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -5,6 +5,8 @@
 # Configure parameters to be filtered from the log file. Use this to limit dissemination of
 # sensitive information. See the ActiveSupport::ParameterFilter documentation for supported
 # notations and behaviors.
+
+# TODO: Reassess removing token
 Rails.application.config.filter_parameters += %i[
-  passw secret token _key crypt salt certificate otp ssn
+  passw secret _key crypt salt certificate otp ssn
 ]
--- lib/scripts/clear_duplicates_on_historic_rates.rb
+++ lib/scripts/clear_duplicates_on_historic_rates.rb
@@ -2,10 +2,10 @@
 
 # Removes duplicates on historic rates
 duplicates_query = <<~SQL
-  SELECT hr.token_id, hr.date, COUNT(hr)
-  FROM historic_rates hr
-  GROUP BY hr.token_id, hr.date
-  HAVING COUNT(hr) > 1
+  SELECT hr.token_id, date_part('year', hr.date) AS YEAR, date_part('month', hr.date) AS MONTH, date_part('day', hr.date) AS DAY, date_part('hour', hr.date) AS HOUR, date_part('minute', hr.date) AS MINUTE, ROUND(date_part('second', hr.date)) AS SECOND, COUNT(hr)
+      FROM historic_rates hr
+      GROUP BY hr.token_id, YEAR, MONTH, DAY, HOUR, MINUTE, SECOND
+      HAVING COUNT(hr) > 1
 SQL
 
 ActiveRecord::Base.connection.execute(duplicates_query).each { |x| Rails.logger.debug x }
@@ -13,7 +13,12 @@ ActiveRecord::Base.connection.execute(duplicates_query).each { |x| Rails.logger.
 remove_duplicates_query = <<~SQL
   DELETE FROM historic_rates a USING historic_rates b
   WHERE a.id < b.id
-  AND a.date = b.date
+  AND date_part('year', a.date) = date_part('year', b.date)
+  AND date_part('month', a.date) = date_part('month', b.date)
+  AND date_part('day', a.date) = date_part('day', b.date)
+  AND date_part('hour', a.date) = date_part('hour', b.date)
+  AND date_part('minute', a.date) = date_part('minute', b.date)
+  AND ROUND(date_part('second', a.date)) = ROUND(date_part('second', b.date))
   AND a.token_id = b.token_id
 SQL
 ActiveRecord::Base.connection.execute(remove_duplicates_query)
