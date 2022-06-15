2dedd1656fab28eb5764c7e7fda1e9c787e81406
Pedro Costa <40130589+pCosta99@users.noreply.github.com>
2022-06-15 19:11:12 +0100

    Remove unnecessary build files (#23)
    * remove ridiculous amounts of code from here
    
    * fix actions
    
    * change order of actions
    
    * fix esbuild.config.js

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,33 +1,43 @@
 #!/usr/bin/env node
 
 const path = require('path')
-const chokidar = require('chokidar')
-const http = require('http')
+const esbuild = require('esbuild')
+const rails = require('esbuild-rails')
 
 const clients = []
 
-http.createServer((req, res) => {
-  return clients.push(
-    res.writeHead(200, {
-      "Content-Type": "text/event-stream",
-      "Cache-Control": "no-cache",
-      "Access-Control-Allow-Origin": "*",
-      Connection: "keep-alive",
-    }),
-  );
-}).listen(8082);
+const config = {
+  absWorkingDir: path.join(process.cwd(), "app/javascript"),
+  bundle: true,
+  entryPoints: ["application.js"],
+  outdir: path.join(process.cwd(), "app/assets/builds"),
+  plugins: [rails()],
+  sourcemap: process.env.RAILS_ENV != "production"
+};
 
 async function builder() {
+  const chokidar = require('chokidar')
+  const http = require('http')
+
+  http.createServer((req, res) => {
+    return clients.push(
+      res.writeHead(200, {
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+        "Access-Control-Allow-Origin": "*",
+        Connection: "keep-alive",
+      }),
+    );
+  }).listen(8082);
+
   let result = await require("esbuild").build({
-    entryPoints: ["application.js"],
-    bundle: true,
-    outdir: path.join(process.cwd(), "app/assets/builds"),
-    absWorkingDir: path.join(process.cwd(), "app/javascript"),
+    ...config,
     incremental: true,
     banner: {
       js: ' (() => new EventSource("http://localhost:8082").onmessage = () => location.reload())();',
-    },
+    }
   })
+
   chokidar.watch(["./app/javascript/**/*.js", "./app/views/**/*.html.erb", "./app/assets/stylesheets/*.css"]).on('all', (event, path) => {
     if (path.includes("javascript")) {
       result.rebuild()
@@ -36,4 +46,40 @@ async function builder() {
     clients.length = 0
   });
 }
-builder()
+
+console.log(process.argv, "argv")
+console.log(process.argv.includes("--watch"), "includes watch")
+
+if (process.argv.includes("--watch")) {
+  builder()
+} else {
+  esbuild.build({
+    ...config,
+    minify: process.env.RAILS_ENV == "production",
+  }).catch(() => process.exit(1));
+}
+
+if (process.argv.includes("--reload")) {
+  buildAndReload()
+
+} else if (process.argv.includes("--watch")) {
+  // Watch uses esbuild's watch option
+  const watch = process.argv.includes("--watch") && {
+    onRebuild(error) {
+      if (error) console.error("[watch] build failed", error);
+      else console.log("[watch] build finished");
+    },
+  };
+
+  esbuild.build({
+    ...config,
+    watch: watch,
+  }).catch(() => process.exit(1));
+
+} else {
+  // Standard esbuild
+  esbuild.build({
+    ...config,
+    minify: process.env.RAILS_ENV == "production",
+  }).catch(() => process.exit(1));
+}
